c496c48a194f0a89f254ee229f5719de8512bfb4
腾阳 <40232478+TengYang111@users.noreply.github.com>
2018-08-02 18:29:15 +0800

    Add files via upload

---
+++ mingyan/mingyan/pipelines.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import codecs
+import json
+# Define your item pipelines here
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+class MingyanPipeline(object):
+    def __init__(self):
+        self.file=codecs.open('t.json','wb','utf-8')
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + '\n'
+        self.file.write(line.decode('unicode_escape'))
+        return item
